0b3709d3197afae7f62ae1cdadd5db0fa40d96d5
mradul2 <theroyalpekka@gmail.com>
2022-03-01 22:00:48 +0530

    Minor changes in trim.bash

--- yt_scraping/trim.bash
+++ yt_scraping/trim.bash
@@ -1,10 +1,17 @@
 #!/bin/bash
 MODE=$1
 DATA_DIR=$2
+DATA_DIR_10=${DATA_DIR}_10
 KINETICS400_DIR=$DATA_DIR/kinetics400
 KINETICS600_DIR=$DATA_DIR/kinetics600
+KINETICS400_DIR_10=$DATA_DIR_10/kinetics400
+KINETICS600_DIR_10=$DATA_DIR_10/kinetics600
 
 echo "MODE: $MODE"
+echo "Creating directories..."
+mkdir -p $DATA_DIR_100
+mkdir -p $KINETICS400_DIR_10
+mkdir -p $KINETICS600_DIR_10
 
 if [ $MODE = "kinetics400" ]; then
     cat kinetics_400_labels.csv|while read line
@@ -12,7 +19,7 @@ if [ $MODE = "kinetics400" ]; then
         while IFS=, read -r col1 col2
         do
             echo "Trimming videos for $col2"
-            OUT_DATA_DIR=${KINETICS400_DIR}/${col2}_trimmed
+            OUT_DATA_DIR=${KINETICS400_DIR_10}/${col2}
             mkdir -p $OUT_DATA_DIR
             for video in $(ls -A1 -U ${KINETICS400_DIR}/${col2}/*)
                 do
@@ -29,7 +36,7 @@ elif [ $MODE = "kinetics600" ]; then
         while IFS=, read -r col1 col2
         do
             echo "Trimming videos for $col2"
-            OUT_DATA_DIR=${KINETICS600_DIR}/${col2}_trimmed
+            OUT_DATA_DIR=${KINETICS600_DIR_10}/${col2}
             mkdir -p $OUT_DATA_DIR
             for video in $(ls -A1 -U ${KINETICS600_DIR}/${col2}/*)
                 do
