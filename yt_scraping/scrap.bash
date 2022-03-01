335807d7c1bddcab470525ca7068a72ba9893fe6
mradul2 <theroyalpekka@gmail.com>
2022-03-01 19:48:06 +0530

    Remove ffmpeg dependencies for download

--- yt_scraping/scrap.bash
+++ yt_scraping/scrap.bash
@@ -1,41 +1,45 @@
 #!/bin/bash
-NUM_VIDEOS=$1
-LENGTH=$2
+MODE=$1
+NUM_VIDEOS=$2
 DATA_DIR=data
 KINETICS400_DIR=$DATA_DIR/kinetics400
 KINETICS600_DIR=$DATA_DIR/kinetics600
 
+echo "MODE: $MODE"
 echo "Length of videos: $LENGTH"
-echo "Number of videos: $NUM_VIDEOS"
 
-echo "Creating directories..."
-mkdir -p $KINETICS400_DIR
-mkdir -p $KINETICS600_DIR
-
-echo "Downloading Kinetics-400 videos"
-cat kinetics_400_labels.csv|while read line
-do
-    while IFS=, read -r col1 col2
+if [ $MODE = "kinetics400" ]; then
+    echo "Creating directories..."
+    mkdir -p $KINETICS400_DIR
+    echo "Downloading Kinetics-400 videos"
+    cat kinetics_400_labels.csv|while read line
     do
-        echo "Downloading videos for $col2"
-        # Create a directory named col2
-        mkdir -p $KINETICS400_DIR/$col2
-        # Download the videos
-        yt-dlp --max-downloads $NUM_VIDEOS -P $KINETICS400_DIR/$col2 --match-filter "duration>45" --downloader ffmpeg --downloader-args "ffmpeg_i:-ss 00:00:10 -t 00:00:10" "ytsearchall:$col2" 
+        while IFS=, read -r col1 col2
+        do
+            echo "Downloading videos for $col2"
+            # Create a directory named col2
+            mkdir -p $KINETICS400_DIR/$col2
+            # Download the videos
+            yt-dlp --max-downloads $NUM_VIDEOS -P $KINETICS400_DIR/$col2 --match-filter "duration>45 & duration<180" "ytsearchall:$col2" 
+        done
     done
-done
-
-echo "Downloading Kinetics-600 videos"
-cat kinetics_600_labels.csv|while read line
-do
-    while IFS=, read -r col1 col2
+elif [ $MODE = "kinetics600" ]; then
+    echo "Creating directories..."
+    mkdir -p $KINETICS600_DIR
+    echo "Downloading Kinetics-600 videos"
+    cat kinetics_600_labels.csv|while read line
     do
-        echo "Downloading videos for $col2"
-        # Create a directory named col2
-        mkdir -p $KINETICS600_DIR/$col2
-        # Download the videos
-        yt-dlp --max-downloads $NUM_VIDEOS -P $KINETICS600_DIR/$col2 --match-filter "duration>45" --downloader ffmpeg --downloader-args "ffmpeg_i:-ss 00:00:10 -t 00:00:10" "ytsearchall:$col2" 
+        while IFS=, read -r col1 col2
+        do
+            echo "Downloading videos for $col2"
+            # Create a directory named col2
+            mkdir -p $KINETICS600_DIR/$col2
+            # Download the videos
+            yt-dlp --max-downloads $NUM_VIDEOS -P $KINETICS600_DIR/$col2 --match-filter "duration>45 & duration<180" "ytsearchall:$col2" 
+        done
     done
-done
+else
+    echo "Invalid mode"
+fi
 
 echo "Done!"
