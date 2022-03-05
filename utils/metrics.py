fa205d4e70eb1541b73d9215cc96113562c5f92c
Animesh Jha <jha.animesh01@gmail.com>
2022-03-05 16:37:05 +0530

    set seed

--- utils/metrics.py
+++ utils/metrics.py
@@ -4,7 +4,23 @@
 import torch
 from torchmetrics.functional import accuracy
 import torch.nn.functional as F
+import numpy as np
+import random as rn
+import pytorch_lightning as pl
+import tensorflow as tf
 
+
+SEED_ = 42
+def seed_all(SEED=SEED_):
+    rn.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+    torch.backends.cudnn.deterministic = True
+    pl.seed_everything(SEED)
+    tf.random.set_seed(SEED)
+
+seed_all(SEED_)
 
 def topk_accuracy(preds, target, k=5):
     """
