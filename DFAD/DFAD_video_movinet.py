86a37fe49ced46e25f7c3d3c94d693be6c744bd9
Ishan Manchanda <ishanmanchanda70@gmail.com>
2022-03-11 11:06:27 +0530

    Prevent TF from hogging all memory

--- DFAD/DFAD_video_movinet.py
+++ DFAD/DFAD_video_movinet.py
@@ -196,6 +196,18 @@ def main():
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
     if args.model_name == "movinet":
+        gpus = tf.config.list_physical_devices('GPU')
+        if gpus:
+            try:
+                # Currently, memory growth needs to be the same across GPUs
+                for gpu in gpus:
+                    tf.config.experimental.set_memory_growth(gpu, True)
+                logical_gpus = tf.config.list_logical_devices('GPU')
+                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+            except RuntimeError as e:
+                # Memory growth must be set before GPUs have been initialized
+                print(e)
+                
         print("\n######################## Loading Model ########################\n")
         hub_url = "https://tfhub.dev/tensorflow/movinet/a2/base/kinetics-600/classification/3"
 
