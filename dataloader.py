9e818d5a0f7489032e7645d1d6e48eba2dcddde8
shubhraneel <shubhraneelpal@gmail.com>
2022-03-12 22:10:02 +0530

    minor change

--- dataloader.py
+++ dataloader.py
@@ -28,14 +28,14 @@ class SwinTransform:
         x = x*n + x[:(self.total_frames % len(x))]
         assert len(x) == self.total_frames, "clip length mismatch"
         video_transform = []
-        for image in x:
+        for im in x:
             im = im.permute(2, 0, 1)
             im = TF.resize(im, 224)
             im = TF.center_crop(im, (224, 224))
             im = im.float()
             im = TF.normalize(im, [123.675, 116.28, 103.53], [58.395, 57.12, 57.375])
             im = im.permute(1, 2, 0)
-            video_transform.append(image)
+            video_transform.append(im)
         vid = torch.stack(video_transform)
         vid = vid.reshape((-1, self.n_clips, self.clip_len, 224, 224, 3))
         vid = vid.permute(0, 1, 5, 2, 3, 4)
@@ -54,14 +54,14 @@ class MovinetTransform:
     def transform(self, vid):
         x = list(vid)
         video_transform = []
-        for image in x:
+        for im in x:
             im = im.permute(2, 0, 1)
             im = TF.resize(im, 224)
             im = TF.center_crop(im, (224, 224))
             im = im.float()
             im = im/255.0
             im = im.permute(1, 2, 0)
-            video_transform.append(image)
+            video_transform.append(im)
         vid = torch.stack(video_transform)
         return vid
 
