a4348bac9c4c51c9aba6b609b9eb2c0a46f570ef
Ishan Manchanda <ishanmanchanda70@gmail.com>
2022-03-13 18:30:35 +0530

    Added temp hardcoded ngpu

--- DFAD/DFAD_video.py
+++ DFAD/DFAD_video.py
@@ -267,7 +267,7 @@ def main():
     # NOTE: Lightweight CNN model (Mobile Net)
     student = torchvision.models.mobilenet_v2()
     print("\nLoaded student and teacher")
-    generator = network.models.ImageGenerator(nz=args.nz)
+    generator = network.models.ImageGenerator(ngpu=1, nz=args.nz)
     print("Loaded student, generator and teacher\n")
 
     if args.wandb:
