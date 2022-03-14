141b0b6f5063bcb1fe05bc4aad2e35b0eadd363a
Ishan Manchanda <ishanmanchanda70@gmail.com>
2022-03-14 23:24:24 +0530

    Swapped ReLU for LReLU

--- DFAD/network/models.py
+++ DFAD/network/models.py
@@ -186,19 +186,19 @@ class ImageGenerator(nn.Module):
             # input is Z, going into a convolution
             nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 8),
-            nn.ReLU(True),
+            nn.LeakyReLU(inplace=True),
             # state size. (ngf*8) x 4 x 4
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
-            nn.ReLU(True),
+            nn.LeakyReLU(inplace=True),
             # state size. (ngf*4) x 8 x 8
             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
-            nn.ReLU(True),
+            nn.LeakyReLU(inplace=True),
             # state size. (ngf*2) x 16 x 16
             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
-            nn.ReLU(True),
+            nn.LeakyReLU(inplace=True),
             nn.ConvTranspose2d(ngf, nc, kernel_size=1, stride=1, padding=0, bias=False),
         )
 
