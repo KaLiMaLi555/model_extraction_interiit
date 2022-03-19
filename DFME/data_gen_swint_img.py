85c0723b8ca58a25dc5af0f6a05c136e37e86c9d
Manideep Sridhara <73156028+manideep1108@users.noreply.github.com>
2022-03-19 14:44:15 +0530

    Update data_gen_swint_img.py

--- DFME/data_gen_swint_img.py
+++ DFME/data_gen_swint_img.py
@@ -62,7 +62,7 @@ def gen_examples(args, generator, teacher, device, epoch=None):
     distribution = []
     logs = []
     labs = []
-    for i in tqdm(range(2), position=0, leave=True):
+    for i in tqdm(range(500), position=0, leave=True):
         labels = torch.argmax(torch.randn((args.batch_size, args.num_classes)), dim=1).to(device)
         labels_oh = torch.nn.functional.one_hot(labels, args.num_classes)
         z = torch.randn((args.batch_size, args.nz)).to(device)
