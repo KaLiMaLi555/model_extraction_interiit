d64869e18f754e8cbb25893a12c69a2bbfdfb583
Ishan Manchanda <ishanmanchanda70@gmail.com>
2022-03-21 21:34:12 +0530

    Added approximate grads for swint

--- prod/BlackBox/DFME/models.py
+++ prod/BlackBox/DFME/models.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 import torchvision
 
 
-# MobileNet-v2 with last layer swapped
+# MobileNet-v2 with last layer replaced
 def get_mobilenet(num_classes):
     mobilenet = torchvision.models.mobilenet_v2()
     final_layer = torch.nn.Linear(in_features=mobilenet.classifier[1].in_features, out_features=num_classes)
