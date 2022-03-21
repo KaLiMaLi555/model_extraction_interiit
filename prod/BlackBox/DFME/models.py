0be73b067b5ba03c28c96e9c0053507e34a621d2
Ishan Manchanda <ishanmanchanda70@gmail.com>
2022-03-21 19:58:16 +0530

    Added models, some skeleton stuff

--- prod/BlackBox/DFME/models.py
+++ prod/BlackBox/DFME/models.py
@@ -1,1 +1,195 @@
-# ImageGenerator and VideoGAN, Student
+# Import the required libraries
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+
+# MobileNet-v2 with last layer swapped
+def get_mobilenet(num_classes):
+    mobilenet = torchvision.models.mobilenet_v2()
+    final_layer = torch.nn.Linear(in_features=mobilenet.classifier[1].in_features, out_features=num_classes)
+    mobilenet.classifier[1] = final_layer
+    return mobilenet
+
+
+# 2D CNN encoder using ResNet-50 architecture
+class ResCNNEncoder(nn.Module):
+    def __init__(self, fc_hidden1=512, fc_hidden2=512, drop_p=0.3, CNN_embed_dim=300):
+        """Load the ResNet-152 architecture and replace top fc layer."""
+        super(ResCNNEncoder, self).__init__()
+
+        self.fc_hidden1, self.fc_hidden2 = fc_hidden1, fc_hidden2
+        self.drop_p = drop_p
+
+        resnet = torchvision.models.resnet50(pretrained=False)
+        modules = list(resnet.children())[:-1]  # delete the last fc layer.
+        self.resnet = nn.Sequential(*modules)
+        self.fc1 = nn.Linear(resnet.fc.in_features, fc_hidden1)
+        self.bn1 = nn.BatchNorm1d(fc_hidden1, momentum=0.01)
+        self.fc2 = nn.Linear(fc_hidden1, fc_hidden2)
+        self.bn2 = nn.BatchNorm1d(fc_hidden2, momentum=0.01)
+        self.fc3 = nn.Linear(fc_hidden2, CNN_embed_dim)
+
+    def forward(self, x_3d):
+        cnn_embed_seq = []
+        for t in range(x_3d.size(1)):
+            # ResNet CNN
+            x = x_3d[:, t, :, :, :]
+            # NOTE: Uncomment when the shape of input video is (b, c, f, h, w)
+            # x = x.reshape((-1, x.shape[3], x.shape[1], x.shape[2]))
+
+            x = self.resnet(x)  # ResNet
+            x = x.view(x.size(0), -1)  # flatten output of conv
+
+            # FC layers
+            x = self.bn1(self.fc1(x))
+            x = F.relu(x)
+            x = self.bn2(self.fc2(x))
+            x = F.relu(x)
+            x = self.fc3(x)
+
+            cnn_embed_seq.append(x)
+
+        # Swap time and sample dim such that (sample dim, time dim, CNN latent dim)
+        cnn_embed_seq = torch.stack(cnn_embed_seq, dim=0).transpose_(0, 1)
+        # cnn_embed_seq: shape (batch, time_step, input_size)
+        return cnn_embed_seq
+
+
+# LSTM Decoder
+class LSTMDecoder(nn.Module):
+    def __init__(self, CNN_embed_dim=300, h_RNN_layers=3, h_RNN=256, h_FC_dim=128, drop_p=0.3, num_classes=400):
+        super(LSTMDecoder, self).__init__()
+
+        self.RNN_input_size = CNN_embed_dim
+        self.h_RNN_layers = h_RNN_layers  # RNN hidden layers
+        self.h_RNN = h_RNN  # RNN hidden nodes
+        self.h_FC_dim = h_FC_dim
+        self.drop_p = drop_p
+        self.num_classes = num_classes
+
+        self.LSTM = nn.LSTM(
+            input_size=self.RNN_input_size,
+            hidden_size=self.h_RNN,
+            num_layers=h_RNN_layers,
+            batch_first=True,  # input & output will have batch size as 1st dimension. Eg: (batch, time_step, input_size)
+        )
+
+        self.fc1 = nn.Linear(self.h_RNN, self.h_FC_dim)
+        self.fc2 = nn.Linear(self.h_FC_dim, self.num_classes)
+
+    def forward(self, x_RNN):
+        self.LSTM.flatten_parameters()
+        # h_n shape (n_layers, batch, hidden_size), h_c shape (n_layers, batch, hidden_size)
+        # None represents zero initial hidden state. RNN_out has shape (batch, time_step, output_size)
+        RNN_out, (h_n, h_c) = self.LSTM(x_RNN, None)
+
+        # FC layers
+        x = self.fc1(RNN_out[:, -1, :])  # choose RNN_out at the last time step
+        x = F.relu(x)
+        x = F.dropout(x, p=self.drop_p, training=self.training)
+        x = self.fc2(x)
+
+        return x
+
+
+# ResCNN-LSTM model for video classification
+class ResCNNLSTM(nn.Module):
+    def __init__(self, fc_hidden1=512, fc_hidden2=512, drop_p=0.3, CNN_embed_dim=300, h_RNN_layers=3, h_RNN=256, h_FC_dim=128, num_classes=400):
+        """Load the ResNet-152"""
+        super(ResCNNLSTM, self).__init__()
+
+        self.encoder = ResCNNEncoder(fc_hidden1=fc_hidden1, fc_hidden2=fc_hidden2, drop_p=drop_p, CNN_embed_dim=CNN_embed_dim)
+        self.decoder = LSTMDecoder(CNN_embed_dim=CNN_embed_dim, h_RNN_layers=h_RNN_layers, h_RNN=h_RNN, h_FC_dim=h_FC_dim, drop_p=drop_p, num_classes=num_classes)
+
+    def forward(self, x_3d):
+        return self.decoder(self.encoder(x_3d))
+
+
+# VideoGAN with background process removed
+class VideoGAN(nn.Module):
+    def __init__(self, zdim=100):
+        super(VideoGAN, self).__init__()
+
+        self.zdim = zdim
+
+        # Foreground
+        self.conv1 = nn.ConvTranspose3d(zdim, 512, [1, 7, 7], [1, 1, 1])
+        self.bn1 = nn.BatchNorm3d(512)
+
+        self.conv2 = nn.ConvTranspose3d(512, 256, [1, 4, 4], [1, 2, 2], [0, 1, 1])
+        self.bn2 = nn.BatchNorm3d(256)
+
+        self.conv3 = nn.ConvTranspose3d(256, 128, [4, 4, 4], [2, 2, 2], [1, 1, 1])
+        self.bn3 = nn.BatchNorm3d(128)
+
+        self.conv4 = nn.ConvTranspose3d(128, 64, [4, 4, 4], [2, 2, 2], [1, 1, 1])
+        self.bn4 = nn.BatchNorm3d(64)
+
+        self.conv5 = nn.ConvTranspose3d(64, 32, [4, 4, 4], [2, 2, 2], [1, 1, 1])
+        self.bn5 = nn.BatchNorm3d(32)
+
+        self.conv6 = nn.ConvTranspose3d(32, 3, [4, 4, 4], [2, 2, 2], [1, 1, 1])
+
+        # Init weights
+        for m in self.modules():
+            classname = m.__class__.__name__
+            if classname.lower().find('conv') != -1:
+                nn.init.normal_(m.weight, mean=0, std=0.01)
+                nn.init.constant_(m.bias, 0)
+            elif classname.lower().find('bn') != -1:
+                nn.init.normal_(m.weight, mean=1, std=0.02)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, z):
+        # Foreground
+        z_unsqueezed = z.unsqueeze(2).unsqueeze(3).unsqueeze(4)
+        f = F.leaky_relu(self.bn1(self.conv1(z_unsqueezed)))
+        f = F.leaky_relu(self.bn2(self.conv2(f)))
+        f = F.leaky_relu(self.bn3(self.conv3(f)))
+        f = F.leaky_relu(self.bn4(self.conv4(f)))
+        f = F.leaky_relu(self.bn5(self.conv5(f)))
+
+        out = self.conv6(f)  # b, 3, 32, 64, 64
+        return out
+
+
+# REVIEW: Use DCGAN instead? Would it be easier to defend?
+#   Will be able to give a sense of the size of the model through name only,
+#   can be a good thing or a bad thing.
+# ImageGenerator
+class ImageGenerator(nn.Module):
+    def __init__(self, ngpu, nc=3, nz=100, ngf=64):
+        super(ImageGenerator, self).__init__()
+        self.ngpu = ngpu
+
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.LeakyReLU(inplace=True),
+            # State size: (ngf*8) x 4 x 4
+
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.LeakyReLU(inplace=True),
+            # State size: (ngf*4) x 8 x 8
+
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.LeakyReLU(inplace=True),
+            # State size: (ngf*2) x 16 x 16
+
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.LeakyReLU(inplace=True),
+            nn.ConvTranspose2d(ngf, nc, kernel_size=1, stride=1, padding=0, bias=False),
+        )
+
+    def forward(self, x):
+        if x.is_cuda and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, x, range(self.ngpu))
+        else:
+            output = self.main(x)
+        return output
